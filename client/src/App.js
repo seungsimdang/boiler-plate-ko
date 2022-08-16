41f4601bdfabde47dd94d2da3cb34a97b161db15
SeungHyun Lee <93538221+222SeungHyun@users.noreply.github.com>
2022-08-16 22:37:53 +0900

    [FEAT] Add React Router Dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
+import LandingPage from './/components/views/LandingPage/LandingPage';
+import LoginPage from './/components/views/LoginPage/LoginPage';
+import RegisterPage from './/components/views/RegisterPage/RegisterPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <hr />
+
+        <Routes>
+          <Route exact path="/" element={<LandingPage />} />
+          <Route exact path="/login" element={<LoginPage />} />
+          <Route exact path="/register" element={<RegisterPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function LandingPage() {
-  return <div>LandingPage</div>;
+  return <div>LandingPage 랜딩페이지</div>;
 }
 
 export default LandingPage;
